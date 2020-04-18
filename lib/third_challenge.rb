762e55c415b044eb72f485a8c1ada0c97fd8f959
Michael Nnaji <mnnaji3@uwo.ca>
2020-04-18 23:46:30 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -21,7 +21,7 @@ def third_challenge
    },
    :capulet => {
       :patriarch => {
-        name: "Lord capulet",
+        name: "Lord Capulet",
         age: "50"
         #your key/value pairs here
         },
