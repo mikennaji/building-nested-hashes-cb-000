36cf70bc44c33fbaed2c3e5edd94dcc125f5aac9
Michael Nnaji <mnnaji3@uwo.ca>
2020-04-18 23:55:35 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -7,9 +7,9 @@ def fourth_challenge
       :hero_friends => [{
         name: "Benvolio",
         age: "17",
-        attitude: "worried"}, {  name: "Benvolio",
-          age: "17",
-          attitude: "worried"}]
+        attitude: "worried"}, {  name: "Mercutio",
+          age: "18",
+          attitude: "hot-headed"}]
         #your hashes here!
 
    },
