46f66ea34ab5bd002489ee587a730098c69ecbbc
Michael Nnaji <mnnaji3@uwo.ca>
2020-04-18 23:45:27 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,23 +3,37 @@ def third_challenge
    :montague => {
       :patriarch => {
         #your key/value pairs here
+        name: "Lord Montague",
+        age: "53"
         },
       :matriarch => {
+        name: "Lady Montague",
+        age: "54"
         #your key/value pairs here
         },
       :hero => {
+        name: "Romeo",
+        age: "15",
+        status: "alive"
         #your key/value pairs here
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
+        name: "Lord capulet",
+        age: "51"
         #your key/value pairs here
         },
       :matriarch => {
+        name: "Lady Capulet",
+        age: "51"  
         #your key/value pairs here
         },
       :heroine => {
+        name: "Juliet",
+        age: "15",
+        status: "alive"
         #your key/value pairs here
         },
       :heroine_friends => []
