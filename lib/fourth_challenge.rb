acd07b46a99f5a28f11d0deeed89e6250cb59ecb
Michael Nnaji <mnnaji3@uwo.ca>
2020-04-18 23:53:05 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,7 +4,12 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
+      :hero_friends => [{
+        name: "Benvolio",
+        age: "17",
+        attitude: "worried"}, {  name: "Benvolio",
+          age: "17",
+          attitude: "worried"}]
         #your hashes here!
       ]
    }, 
@@ -13,7 +18,12 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        #your hashes here!{
+          name: "Steven",
+          age: "30",
+          attitude: "confused"}, {  name: "Nurse",
+            age: "44",
+            attitude: "worried"}
       ]
    }
 }
